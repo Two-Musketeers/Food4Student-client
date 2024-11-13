3dc26bfe7fc50f23761dd0647b394065e2472131
Nguyên Minh Hồ <honguyenminh@outlook.com>
2024-11-13 23:12:28 +0700

    Split main screen nav graph

--- app/src/main/java/com/ilikeincest/food4student/screen/main_page/MainScreen.kt
+++ app/src/main/java/com/ilikeincest/food4student/screen/main_page/MainScreen.kt
@@ -1,29 +1,15 @@
 package com.ilikeincest.food4student.screen.main_page
 
 import android.widget.Toast
-import androidx.annotation.StringRes
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateIntOffsetAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ReceiptLong
-import androidx.compose.material.icons.automirrored.outlined.ReceiptLong
-import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.Checklist
-import androidx.compose.material.icons.filled.Fastfood
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.outlined.BookmarkBorder
-import androidx.compose.material.icons.outlined.Fastfood
-import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -47,43 +33,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.ilikeincest.food4student.R
 import com.ilikeincest.food4student.screen.main_page.component.GlobalSearchBar
-import com.ilikeincest.food4student.screen.main_page.order.OrderScreen
-
-private enum class MainRoutes(
-    @StringRes val labelResId: Int,
-    val unselectedIcon: @Composable () -> Unit,
-    val selectedIcon: @Composable () -> Unit
-) {
-    HOME(
-        R.string.home_screen_label,
-        { Icon(Icons.Outlined.Fastfood, null) },
-        { Icon(Icons.Filled.Fastfood, null) }
-    ),
-    ORDER(
-        R.string.order_screen_label,
-        { Icon(Icons.AutoMirrored.Outlined.ReceiptLong, null) },
-        { Icon(Icons.AutoMirrored.Filled.ReceiptLong, null) }
-    ),
-    FAVORITE(
-        R.string.favorite_screen_label,
-        // This is utterly stupid. Fuck google. And their stupid ass everything.
-        // Why the fuck is Icons.Outlined.Bookmark the same as Icons.Filled.Bookmark???
-        // Then what the fuck does Outlined even mean???
-        { Icon(Icons.Outlined.BookmarkBorder, null) },
-        { Icon(Icons.Filled.Bookmark, null) },
-    ),
-    NOTIFICATION(
-        R.string.notification_screen_label,
-        { Icon(Icons.Outlined.Notifications, null) },
-        { Icon(Icons.Filled.Notifications, null) }
-    ),
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -188,42 +140,17 @@ fun MainScreen(
             onExpandedChange = { expanded = it },
             isVisible = animatedSearchBarOffset != IntOffset(0, 0)
         )
+
         // Main screen content
-        val inTransition = fadeIn(tween(durationMillis = 250))
-        val outTransition = fadeOut(tween(durationMillis = 250))
-        NavHost(
+        MainScreenNavGraph(
             navController = navController,
-            startDestination = MainRoutes.HOME.name,
-            enterTransition = { inTransition },
-            popEnterTransition = { inTransition },
-            exitTransition = { outTransition },
-            popExitTransition = { outTransition },
+            onNavigateToMap = onNavigateToMap,
+            onNavigateToAccountCenter = onNavigateToAccountCenter,
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(top = animatedContentPaddingSearchBar)
                 .offset { animatedContentOffset }
-        ) {
-            composable(MainRoutes.HOME.name) {
-                Column {
-                    Text("Home")
-                    Button(onClick = onNavigateToMap) {
-                        Text("Go to map")
-                    }
-                    Button(onClick = onNavigateToAccountCenter) {
-                        Text("Go to account center")
-                    }
-                }
-            }
-            composable(MainRoutes.ORDER.name) {
-                OrderScreen()
-            }
-            composable(MainRoutes.FAVORITE.name) {
-                Text("Favorite")
-            }
-            composable(MainRoutes.NOTIFICATION.name) {
-                Text("Notification")
-            }
-        }
+        )
     }
 }
 
